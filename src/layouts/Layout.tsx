ea47a3ce519477652c659f8ac457552636b11985
Marc Mansour <marcm9387@gmail.com>
2024-08-30 18:35:21 +0200

    adding padding on smaller screen

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex-1 py-10">{children}</div>
+      <div className="flex-1 py-10 px-4 sm:px-6 lg:px-8">{children}</div>
       <Footer />
     </div>
   );
