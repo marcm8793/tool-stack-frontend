17029bd8b3ed585d6978dd933872be9ff96ff320
Marc Mansour <marcm9387@gmail.com>
2025-01-04 18:10:42 +0100

    Remove PostHog integration and related dependencies; add Umami analytics script to index.html

--- src/main.tsx
+++ src/main.tsx
@@ -5,24 +5,14 @@ import AppRoutes from "./AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
 import { ThemeProvider } from "./components/navbar/theme-provider";
-import { PostHogProvider } from "posthog-js/react";
-
-const options = {
-  api_host: import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_HOST,
-};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_REACT_APP_PUBLIC_POSTHOG_KEY}
-      options={options}
-    >
-      <Router>
-        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-          <AppRoutes />
-          <Toaster />
-        </ThemeProvider>
-      </Router>
-    </PostHogProvider>
+    <Router>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <AppRoutes />
+        <Toaster />
+      </ThemeProvider>
+    </Router>
   </StrictMode>
 );
