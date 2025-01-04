1c69a59a568f9b014e3fa4f9263f5c15d2d901dc
Marc Mansour <marcm9387@gmail.com>
2025-01-04 17:09:48 +0100

    Integrate PostHog for analytics tracking and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,24 @@ import AppRoutes from "./AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
 import { ThemeProvider } from "./components/navbar/theme-provider";
+import { PostHogProvider } from "posthog-js/react";
+
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <AppRoutes />
-        <Toaster />
-      </ThemeProvider>
-    </Router>
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <Router>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <AppRoutes />
+          <Toaster />
+        </ThemeProvider>
+      </Router>
+    </PostHogProvider>
   </StrictMode>
 );
