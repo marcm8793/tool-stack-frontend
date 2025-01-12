4109364219ca479edb1beaf23e1a81fa3c55764a
Marc Mansour <marcm9387@gmail.com>
2025-01-12 21:56:24 +0100

    Integrate PostHog for analytics tracking in production; update package dependencies in package.json and package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,40 @@ import AppRoutes from "./AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
 import { ThemeProvider } from "./components/navbar/theme-provider";
+import { PostHogProvider } from "posthog-js/react";
+import posthog from "posthog-js";
+
+// Only initialize PostHog in production
+if (import.meta.env.PROD) {
+  posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, {
+    api_host: import.meta.env.VITE_POSTHOG_UI_HOST,
+    ui_host: import.meta.env.VITE_POSTHOG_HOST,
+  });
+}
+
+if (import.meta.env.PROD) {
+  console.log("Production mode!");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <AppRoutes />
-        <Toaster />
-      </ThemeProvider>
-    </Router>
+    {/* Conditionally render PostHogProvider only in production */}
+    {import.meta.env.PROD ? (
+      <PostHogProvider client={posthog}>
+        <Router>
+          <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+            <AppRoutes />
+            <Toaster />
+          </ThemeProvider>
+        </Router>
+      </PostHogProvider>
+    ) : (
+      <Router>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <AppRoutes />
+          <Toaster />
+        </ThemeProvider>
+      </Router>
+    )}
   </StrictMode>
 );
